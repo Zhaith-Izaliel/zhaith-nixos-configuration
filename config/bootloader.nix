b0a68d815a219458935288d0c2b39d48e6fdd02f
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2022-10-05 08:55:26 +0200

    feat: update nixos icon in grub

--- config/bootloader.nix
+++ config/bootloader.nix
@@ -2,17 +2,15 @@
 { config, pkgs, ... }:
 
 {
-  # Use efi systemd-boot
+  # Use efi grub
   boot.loader = {
     efi.canTouchEfiVariables = true;
-    #systemd-boot.enable = true;
     grub = {
       enable = true;
       efiSupport = true;
       theme = "/etc/nixos/assets/grub-theme/vimix";
       #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
       device = "nodev";
-      #useOSProber = true;
       copyKernels = true;
       extraEntries = ''
         menuentry "Reboot" --class restart {
@@ -22,6 +20,8 @@
           halt
         }
       '';
+      entryOptions = "--class nixos --unrestricted";
+      subEntryOptions = "--class recovery";
     };
   };
 }
