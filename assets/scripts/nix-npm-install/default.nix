56c80e003d21bebde1175447f9e43a837acb8263
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2022-10-05 08:56:01 +0200

    feat: update npm-install-nix to 1.0.1

--- assets/scripts/nix-npm-install/default.nix
+++ assets/scripts/nix-npm-install/default.nix
@@ -5,13 +5,13 @@ let
 in
 callPackage ../builder.nix rec {
   pname = "nix-npm-install";
-  version = "v1.0.0";
+  version = "v1.0.1";
 
   src = fetchFromGitLab {
     repo = "nix-npm-install";
     owner = "zhaith-izaliel-group/configuration/nixos/nixos-scripts";
     rev = version;
-    sha256 = "11c9a990z1y4d9ka1gh1y60d30crjm2l11gr9l10jkji92cy845l";
+    sha256 = "sha256-A+ghVVQLMRa/jMa2jC0++eCjlgvxpGSccdBS1NziPc8=";
   };
 
   buildInputs = [
