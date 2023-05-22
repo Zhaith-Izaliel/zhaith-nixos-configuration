cb4438b3d6abaeb41a1a53a08b8ac924096b0051
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-05-22 10:59:07 +0200

    fix(Whispering-Willow): tape package

--- hosts/Whispering-Willow/assets/packages/tape/default.nix
+++ hosts/Whispering-Willow/assets/packages/tape/default.nix
@@ -3,20 +3,21 @@
 let
   name = "tape";
   version = "1.4.0";
-  src = pkgs.fetchgitPrivate {
+  src = builtins.fetchGit {
     url = "git@gitlab.com:Zhaith-Izaliel/unfree-packages.git";
-    sha256 = "";
-  }."tape_${version}.AppImage";
+    ref = "master";
+    rev = "9746945bcf8fdc8a372bd5bba6656f09373240d7";
+  } + "/tape_${version}.AppImage";
   appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
 in
 pkgs.appimageTools.wrapType2 {
   inherit name src;
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${name}.desktop
-      \ $out/share/applications/${name}.desktop
-    sed -i 's/.*Exec.*/Exec=${name}/'
-      \ $out/share/applications/ankama-launcher.desktop
+    install -m 444 -D ${appimageContents}/${name}.desktop \
+      $out/share/applications/${name}.desktop
+    sed -i "s/.*Exec.*/Exec=${name}/" \
+      $out/share/applications/${name}.desktop
   '';
 }
 
