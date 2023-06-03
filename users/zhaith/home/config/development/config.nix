cb79ba5c77ddea0dd866b20cf9d97daecf8907ef
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-06-03 14:52:57 +0200

    fix!(zhaith): coq_nvim

--- users/zhaith/home/config/development/config.nix
+++ users/zhaith/home/config/development/config.nix
@@ -3,18 +3,18 @@
 let
   config = pkgs.stdenv.mkDerivation rec {
     name = "neovim-config";
-    version = "1.3.0";
+    version = "1.4.0";
 
     src = pkgs.fetchFromGitLab {
       repo = name;
       owner = "Zhaith-Izaliel";
       rev = "v${version}";
-      sha256 = "sha256-SJ/p3vmGtuMwt4h/2ekjJIYdh05lM/7xNjZyLahfEIg";
+      sha256 = "sha256-FqJyGzk353elQE9PfzNR1PIdzIIUFFIz4eb7hcIBbgg=";
     };
 
     installPhase = ''
-    mkdir -p $out
-    cp -r --target-directory $out lua init.lua
+      mkdir -p $out
+      cp -r --target-directory $out lua init.lua
     '';
   };
 in
