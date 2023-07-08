9925197d4d9e92321286bbcb7a6c0f5f5ea16b44
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-07-08 09:41:09 +0200

    feat: swaylock and swayidle

--- users/zhaith/home/config/hyprland.nix
+++ users/zhaith/home/config/hyprland.nix
@@ -87,6 +87,63 @@ in
     '';
   };
 
+  programs.swaylock = {
+    enable = true;
+    settings = {
+      color = "24273a";
+      bs-hl-color= "f4dbd6";
+      caps-lock-bs-hl-color = "f4dbd6";
+      caps-lock-key-hl-color="a6da95";
+      inside-color = "24273a";
+      inside-clear-color = "24273a";
+      inside-caps-lock-color = "24273a";
+      inside-ver-color = "24273a";
+      inside-wrong-color = "24273a";
+      key-hl-color = "a6da95";
+      layout-bg-color = "00000000";
+      layout-border-color = "00000000";
+      layout-text-color = "cad3f5";
+      line-color = "00000000";
+      line-clear-color="00000000";
+      line-caps-lock-color="00000000";
+      line-ver-color="00000000";
+      line-wrong-color="00000000";
+      ring-color="b7bdf8";
+      ring-clear-color="f4dbd6";
+      ring-caps-lock-color="f5a97f";
+      ring-ver-color="8aadf4";
+      ring-wrong-color="ee99a0";
+      separator-color="00000000";
+      text-color="cad3f5";
+      text-clear-color="f4dbd6";
+      text-caps-lock-color="f5a97f";
+      text-ver-color="8aadf4";
+      text-wrong-color="ee99a0";
+      font-size = gtk-theme.font.size;
+      font = gtk-theme.font.name;
+      indicator-caps-lock = true;
+      image = "/home/zhaith/Pictures/Wallpapers/Nord/cats.png";
+    };
+  };
+
+  services.swayidle = {
+    enable = true;
+    timeouts = [
+      { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
+      {
+        timeout = 600;
+        command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+        resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+      }
+    ];
+    events = [
+      { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
+    ];
+    extraArgs = [
+      "-w"
+    ];
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland = {
--- users/zhaith/home/config/tools/bat.nix
+++ users/zhaith/home/config/tools/bat.nix
@@ -11,7 +11,7 @@
         owner = "catppuccin";
         repo = "bat"; # Bat uses sublime syntax for its themes
         rev = "ba4d168";
-        sha256 = "";
+        sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
       } + "/Catppuccin-macchiato.tmTheme");
     };
     config = {
