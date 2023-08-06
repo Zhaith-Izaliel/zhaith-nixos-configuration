8fb501feac3fff2ee4aeebd9cb4af94e648662c8
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-08-06 21:32:55 +0200

    fix: inadyn

--- hosts/Ethereal-Edelweiss/config/server/inadyn.nix
+++ hosts/Ethereal-Edelweiss/config/server/inadyn.nix
@@ -7,11 +7,11 @@
       period          = 300
       verify-address  = true
 
-      provider default@dyndns.org {
+      provider default@ovh.com {
         ssl         = true
         username    = ethereal-edelweiss.cloud-zhaith
         password    = "@password_placeholder@"
-        hostname    = { nextcloud.ethereal-edelweiss.cloud jellyfin.ethereal-edelweiss.cloud books.ethereal-edelweiss.cloud ethereal-edelweiss.cloud }
+        hostname    = { nextcloud.ethereal-edelweiss.cloud, jellyfin.ethereal-edelweiss.cloud, books.ethereal-edelweiss.cloud, ethereal-edelweiss.cloud }
       }
     '';
     passwordFile = "/var/run/inadyn-password";
--- modules/inadyn.nix
+++ modules/inadyn.nix
@@ -24,25 +24,31 @@ let
   install --mode=600 --owner=$USER ${configText} ${configFile}
   "${pkgs.replace-secret}/bin/replace-secret" "${cfg.passwordPlaceholder}" "${cfg.passwordFile}" "${configFile}"
   '' else "";
+
+  extraArgs = strings.concatStringSep " " ([
+    "--foreground"
+    "--syslog"
+    "--no-pidfile"
+  ] ++ cfg.extraArgs);
 in
-{
-  options.services.inadyn = {
-    enable = mkEnableOption "Inadyn dynamic DNS client";
+  {
+    options.services.inadyn = {
+      enable = mkEnableOption "Inadyn dynamic DNS client";
 
-    package = mkOption {
-      type = types.package;
-      default = pkgs.inadyn;
-      defaultText = literalExpression "pkgs.inadyn";
-      description = ''
+      package = mkOption {
+        type = types.package;
+        default = pkgs.inadyn;
+        defaultText = literalExpression "pkgs.inadyn";
+        description = ''
         Inadyn package to install.
-      '';
-    };
+        '';
+      };
 
-    settings = mkOption {
-      type = types.str;
-      default = "";
-      description = "Configuration options for Inadyn.";
-      example = literalExpression ''
+      settings = mkOption {
+        type = types.str;
+        default = "";
+        description = "Configuration options for Inadyn.";
+        example = literalExpression ''
         period          = 300
         user-agent      = Mozilla/5.0
 
@@ -51,61 +57,50 @@ in
           password    = case-sensitive-pwd
           hostname    = some.example.com
         }
-      '';
-    };
+        '';
+      };
 
-    passwordPlaceholder = mkOption {
-      default = "@password_placeholder@";
-      type = types.nonEmptyStr;
-      description = "The password placeholder to use with the password file.";
-      example = "@secret_password@";
-    };
+      passwordPlaceholder = mkOption {
+        default = "@password_placeholder@";
+        type = types.nonEmptyStr;
+        description = "The password placeholder to use with the password file.";
+        example = "@secret_password@";
+      };
 
-    passwordFile = mkOption {
+      passwordFile = mkOption {
         default = null;
         type = types.nullOr types.str;
         description = lib.mdDoc ''
-          A file containing the password for your `@password_placeholder@` field
+        A file containing the password for your `@password_placeholder@` field
           in `services.inadyn.settings`.
-        '';
-    };
+          '';
+        };
 
-    extraArgs = mkOption {
-      type = types.listOf types.str;
-      default = [
-        "--foreground"
-        "--syslog"
-        "--no-pidfile"
-      ];
-      description = "Extra arguments when running Inadyn.";
-      defaultText = literalExpression ''
-      [
-        "--foreground"
-        "--syslog"
-        "--no-pidfile"
-      ]
-      '';
-      example = literalExpression "--force";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    systemd.services.inadyn = {
-      description = "Internet Dynamic DNS Client";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      requires = [ "network-online.target" ];
-      restartTriggers = optional (cfg.settings != null) configText;
-
-      serviceConfig = {
-        Type = "simple";
-        User = "root";
-        RuntimeDirectoryMode = "0700";
-        inherit RuntimeDirectory StateDirectory;
-        ExecStartPre = "!${pkgs.writeShellScript "inadyn-prestart" preStart}";
-        ExecStart = "${lib.getBin cfg.package}/bin/inadyn -f ${configFile} ${lib.strings.concatStringsSep " " cfg.extraArgs}";
+        extraArgs = mkOption {
+          type = types.listOf types.str;
+          default = [];
+          description = "Extra arguments when running Inadyn.";
+          example = literalExpression "--force";
+        };
       };
-    };
-  };
-}
+
+      config = mkIf cfg.enable {
+        systemd.services.inadyn = {
+          description = "Internet Dynamic DNS Client";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network-online.target" ];
+          requires = [ "network-online.target" ];
+          restartTriggers = optional (cfg.settings != null) configText;
+
+          serviceConfig = {
+            Type = "simple";
+            User = "root";
+            RuntimeDirectoryMode = "0700";
+            inherit RuntimeDirectory StateDirectory;
+            ExecStartPre = "!${pkgs.writeShellScript "inadyn-prestart" preStart}";
+            ExecStart = "${lib.getBin cfg.package}/bin/inadyn -f ${configFile} ${extraArgs}";
+          };
+        };
+      };
+    }
 
