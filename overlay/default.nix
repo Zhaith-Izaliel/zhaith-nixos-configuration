6b6cb7f5462ca53427ccc3fb7a7a3cb4d1285edd
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-02 01:04:36 +0200

    fix: commitlint

--- overlay/default.nix
+++ overlay/default.nix
@@ -12,7 +12,16 @@ in
 
   tape = import ../assets/packages/tape { pkgs = prev; };
 
-  commitlint = nodejs-packages."@commitlint/cli";
+  commitlint = nodejs-packages."@commitlint/cli".overrideAttrs (final:
+    prev: {
+      buildInputs = [ nodejs-packages.commitlint-format-json ] ++ prev.buildInputs;
+      installPhase = prev.installPhase + ''
+
+      mkdir -p $out/node_modules
+      ln -s ${nodejs-packages.commitlint-format-json}/lib/node_modules/* $out/node_modules
+      '';
+    }
+  );
 
   commitlint-config-conventional =
     nodejs-packages."@commitlint/config-conventional";
