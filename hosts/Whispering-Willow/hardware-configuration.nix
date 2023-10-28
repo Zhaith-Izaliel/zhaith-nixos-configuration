65b719e0c92d4ab851fcc0c8ee20b1221ca5fcf2
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-10-28 16:15:53 +0000

    feat!: reduce tex-live size, update hardware configuration after formatting

--- hosts/Whispering-Willow/hardware-configuration.nix
+++ hosts/Whispering-Willow/hardware-configuration.nix
@@ -14,39 +14,29 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2409b750-54cc-4171-a551-e8592292d42f";
+    { device = "/dev/disk/by-uuid/07b0a626-cbfb-4f9e-ae2d-ee488441ceac";
       fsType = "ext4";
-    };
-
-  # fileSystems."/mnt/windows" =
-  #   { device = "/dev/disk/by-uuid/ddae6099-5aec-42a9-a073-2964fcda4f1d";
-  #     fsType = "ntfs";
-  #   };
-  #
-  # fileSystems."/mnt/games" =
-  #   { device = "/dev/disk/by-uuid/1be740f8-ed1b-439b-8f96-f1835e1fbf22";
-  #     fsType = "ntfs";
-  #   };
+    };  
 
   fileSystems."/home" =
     { device = "/dev/disk/by-uuid/9f181965-f48e-4e6e-8dcf-e00695fda38c";
       fsType = "ext4";
-    };
+    };  
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/37C9-C571";
+      fsType = "vfat";
+    };  
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/0e5f0870-489a-455d-bf56-28eccde7c6ce"; }
+    ];
 
   fileSystems."/mnt/games" =
     { device = "/dev/disk/by-uuid/3DC7196A3AF67751";
       fsType = "ntfs-3g";
       options = [ "rw" "uid=1000"];
     };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/37C9-C571";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/0e5f0870-489a-455d-bf56-28eccde7c6ce"; }
-    ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   # high-resolution display
