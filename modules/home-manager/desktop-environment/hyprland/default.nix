c8ad513a9490fb242ee5c24614b979a85422b77e
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-03-07 09:25:51 +0100

    feat!: update flake and nodejs dependencies

--- modules/home-manager/desktop-environment/hyprland/default.nix
+++ modules/home-manager/desktop-environment/hyprland/default.nix
@@ -114,9 +114,9 @@ in {
       GDK_BACKEND = "wayland,x11";
       QT_QPA_PLATFORM = "wayland;xcb";
       CLUTTER_BACKEND = "wayland";
-      XDG_CURRENT_DESKTOP = "Hyprland";
+      # XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_TYPE = "wayland";
-      XDG_SESSION_DESKTOP = "Hyprland";
+      # XDG_SESSION_DESKTOP = "Hyprland";
       WLR_NO_HARDWARE_CURSORS = "1";
     };
 
