20cdc213a029eda019f50b40724a6d4ebe379bb4
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-03-11 09:41:35 +0100

    feat(flake): update flake lock

--- modules/system/kernel.nix
+++ modules/system/kernel.nix
@@ -13,6 +13,6 @@ in {
 
   config = mkIf cfg.enable {
     boot.initrd.kernelModules = ["i915"];
-    boot.kernelPackages = pkgs.linuxPackages_zen;
+    # boot.kernelPackages = pkgs.linuxPackages_zen;
   };
 }
