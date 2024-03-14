371298a00d2529a2e28b9986543a41d3b6320812
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-03-14 14:38:59 +0100

    refactor(packages): refactor packages import in overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,23 +1,24 @@
 {inputs}: final: prev: let
   inherit (packages) nodejs-packages;
   packages = import ../packages {pkgs = final;};
-in {
-  inherit (packages) kando fusion;
-  inherit (nodejs-packages) commitlint-format-json;
+in
+  {
+    inherit (nodejs-packages) commitlint-format-json;
 
-  commitlint = nodejs-packages."@commitlint/cli".overrideAttrs (
-    final: prev: {
-      buildInputs = [nodejs-packages.commitlint-format-json] ++ prev.buildInputs;
-      installPhase =
-        prev.installPhase
-        + ''
+    commitlint = nodejs-packages."@commitlint/cli".overrideAttrs (
+      final: prev: {
+        buildInputs = [nodejs-packages.commitlint-format-json] ++ prev.buildInputs;
+        installPhase =
+          prev.installPhase
+          + ''
 
-          mkdir -p $out/node_modules
-          ln -s ${nodejs-packages.commitlint-format-json}/lib/node_modules/* $out/node_modules
-        '';
-    }
-  );
+            mkdir -p $out/node_modules
+            ln -s ${nodejs-packages.commitlint-format-json}/lib/node_modules/* $out/node_modules
+          '';
+      }
+    );
 
-  commitlint-config-conventional =
-    nodejs-packages."@commitlint/config-conventional";
-}
+    commitlint-config-conventional =
+      nodejs-packages."@commitlint/config-conventional";
+  }
+  // packages
