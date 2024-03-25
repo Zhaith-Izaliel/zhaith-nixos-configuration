5d3a5c5b4e5165002f818ee491b7cb673f042b49
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-03-25 15:45:16 +0100

    fix(packages): import invoiceshelf

--- packages/default.nix
+++ packages/default.nix
@@ -2,4 +2,5 @@
   kando = pkgs.callPackage ./kando.nix {};
   fusion = pkgs.callPackage ./fusion.nix {};
   affine = pkgs.callPackage ./affine.nix {};
+  invoiceshelf = pkgs.callPackage ./invoiceshelf.nix {};
 }
