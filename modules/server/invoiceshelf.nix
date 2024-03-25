51189dc9b4a60d4b239b287bbe3c6e915b50a52a
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-03-25 16:58:20 +0100

    fix(modules/server/invoiceshelf): fix permissions

--- modules/server/invoiceshelf.nix
+++ modules/server/invoiceshelf.nix
@@ -8,6 +8,7 @@
   inherit (lib) mkIf types mkOption mkDefault;
   cfg = config.hellebore.server.invoiceshelf;
   fpm = config.services.phpfpm.pools.invoiceshelf;
+  webserver = config.services.nginx;
   package = pkgs.invoiceshelf.override {dataDir = cfg.dataDir;};
   domain = "${cfg.subdomain}.${config.networking.domain}";
 in {
@@ -54,7 +55,7 @@ in {
       isSystemUser = true;
     };
 
-    users.groups.${cfg.group}.members = [cfg.user config.services.nginx.user];
+    users.groups.${cfg.group}.members = [cfg.user webserver.user];
 
     services.phpfpm.pools.invoiceshelf = {
       phpOptions = ''
@@ -63,14 +64,14 @@ in {
         post_max_size = 64M
       '';
 
-      user = config.services.nginx.user;
-      group = config.services.nginx.group;
+      user = webserver.user;
+      group = webserver.group;
 
       settings =
         {
           "listen.mode" = "${toString cfg.port}";
-          "listen.owner" = config.services.nginx.user;
-          "listen.group" = config.services.nginx.group;
+          "listen.owner" = webserver.user;
+          "listen.group" = webserver.group;
         }
         // cfg.poolConfig;
     };
@@ -83,44 +84,44 @@ in {
       locations = {
         "/" = {
           priority = 1600;
-          proxyPass = "http://localhost:${toString cfg.port}";
-          # tryFiles = "$uri $uri/ /index.php?$query_string";
+          tryFiles = "$uri $uri/ /index.php?$query_string";
         };
 
-        # "~ \\.php$" = {
-        #   priority = 500;
-        #   extraConfig = ''
-        #     fastcgi_pass unix:${fpm.socket};
-        #     fastcgi_index index.php;
-        #     fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        #     include ${config.services.nginx.package}/conf/fastcgi.conf;
-        #   '';
-        # };
+        "~ \\.php$" = {
+          priority = 500;
+          extraConfig = ''
+            fastcgi_pass unix:${fpm.socket};
+            fastcgi_index index.php;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            include ${webserver.package}/conf/fastcgi.conf;
+          '';
+        };
 
-        # "/favicon.ico" = {
-        #   priority = 100;
-        #   extraConfig = ''
-        #     access_log off;
-        #     log_not_found off;
-        #   '';
-        # };
+        "/favicon.ico" = {
+          priority = 100;
+          extraConfig = ''
+            access_log off;
+            log_not_found off;
+          '';
+        };
 
-        # "/robots.txt" = {
-        #   priority = 100;
-        #   extraConfig = ''
-        #     access_log off;
-        #     log_not_found off;
-        #   '';
-        # };
+        "/robots.txt" = {
+          priority = 100;
+          extraConfig = ''
+            access_log off;
+            log_not_found off;
+          '';
+        };
       };
-      # extraConfig = ''
-      #   add_header X-Content-Type-Options nosniff;
-      #   add_header X-XSS-Protection "1; mode=block";
-      #   add_header X-Robots-Tag none;
-      #   add_header Content-Security-Policy "frame-ancestors 'self'";
-      #   index index.php;
-      #   charset utf-8;
-      # '';
+
+      extraConfig = ''
+        add_header X-Content-Type-Options nosniff;
+        add_header X-XSS-Protection "1; mode=block";
+        add_header X-Robots-Tag none;
+        add_header Content-Security-Policy "frame-ancestors 'self'";
+        index index.php;
+        charset utf-8;
+      '';
     };
 
     # security.acme = {
@@ -131,20 +132,20 @@ in {
     # };
 
     systemd.tmpfiles.rules = [
-      "d ${cfg.dataDir}                            0711 ${cfg.user} ${cfg.group} - -"
-      "f ${cfg.dataDir}/database.sqlite            0700 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/public                     0755 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage                    0711 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/app                0711 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/fonts              0700 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/framework          0775 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/framework/cache    0700 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/framework/sessions 0700 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/framework/views    0700 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/storage/logs               0775 ${cfg.user} ${cfg.group} - -"
-      "C ${cfg.dataDir}/.env                       0700 ${cfg.user} ${cfg.group} - ${package}/.env.example"
-      "d ${cfg.dataDir}/bootstrap                  0711 ${cfg.user} ${cfg.group} - -"
-      "d ${cfg.dataDir}/bootstrap/cache            0775 ${cfg.user} ${cfg.group} - -"
+      "d ${cfg.dataDir}                            0711 ${webserver.user} ${webserver.group} - -"
+      "f ${cfg.dataDir}/database.sqlite            0700 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/public                     0755 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage                    0711 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/app                0711 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/fonts              0700 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/framework          0775 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/framework/cache    0700 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/framework/sessions 0700 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/framework/views    0700 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/storage/logs               0775 ${webserver.user} ${webserver.group} - -"
+      "C ${cfg.dataDir}/.env                       0700 ${webserver.user} ${webserver.group} - ${package}/.env.example"
+      "d ${cfg.dataDir}/bootstrap                  0711 ${webserver.user} ${webserver.group} - -"
+      "d ${cfg.dataDir}/bootstrap/cache            0775 ${webserver.user} ${webserver.group} - -"
     ];
   };
 }
