4645337523871b2f2431f9df5d0a9cce84729426
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-04-04 11:49:02 +0200

    fix(modules/server/invoiceshelf): fixing port and container

--- modules/server/invoiceshelf.nix
+++ modules/server/invoiceshelf.nix
@@ -21,31 +21,23 @@ in {
         type = types.nonEmptyStr;
         description = "Defines the user running InvoiceShelf.";
       };
-
-      dbPasswordFile = mkOption {
-        default = "/var/lib/invoiceshelf/db-pass";
-        type = types.nonEmptyStr;
-        description = "File used to store the database password. Must be only readable by root";
-      };
     }
     // extra-types.server-app {
       name = "InvoiceShelf";
       group = "invoiceshelf";
-      port = 80;
+      port = 0660;
     };
 
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers.invoiceshelf = {
       image = "invoiceshelf/invoiceshelf";
 
-      # user = "${cfg.user}:${cfg.group}";
-
       volumes = [
         "./invoiceshelf/data:${cfg.volume}"
       ];
 
       ports = [
-        "90:${toString cfg.port}"
+        "${toString cfg.port}:80"
       ];
 
       environment = {
@@ -56,13 +48,8 @@ in {
         DB_PORT = toString config.services.postgresql.port;
         DB_DATABASE = "invoiceshelf";
         DB_USERNAME = cfg.user;
-        DB_PASSWORD_FILE = cfg.dbPasswordFile;
         STARTUP_DELAY = "30";
       };
-
-      extraOptions = [
-        "--publish"
-      ];
     };
 
     hellebore.server.nginx.enable = mkDefault true;
