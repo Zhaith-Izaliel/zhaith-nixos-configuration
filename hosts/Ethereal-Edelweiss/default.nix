1f0d2cd870e3d1201d1238431241f48a21c6e92e
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-06-01 22:01:10 +0200

    fix(security): remove token

--- hosts/Ethereal-Edelweiss/default.nix
+++ hosts/Ethereal-Edelweiss/default.nix
@@ -75,7 +75,6 @@
         enable = true;
         subdomain = "factorio";
         admins = [];
-        token = "da7b80b5cda8941d879484b2bfe400";
         extraSettingsFile = "/mnt/datas/factorio/secret.json";
         game-name = "Vanilla Maxime-Virgil";
       };
