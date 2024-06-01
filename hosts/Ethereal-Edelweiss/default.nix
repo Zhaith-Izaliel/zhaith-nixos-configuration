d8fe433cd9107587c639d2d7d949689e633c36da
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-06-01 20:06:08 +0200

    fix: update factorio options

--- hosts/Ethereal-Edelweiss/default.nix
+++ hosts/Ethereal-Edelweiss/default.nix
@@ -76,6 +76,8 @@
         subdomain = "factorio";
         admins = [];
         token = "da7b80b5cda8941d879484b2bfe400";
+        extraSettingsFile = lib.cleanSource /mnt/datas/factorio/secret.json;
+        game-name = "Vanilla Maxime-Virgil";
       };
 
       fail2ban = {
