d69cba3efcdbd42d4a6f4e85351fb3227659096d
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-07-02 19:02:16 +0200

    fix(cozy): trying something

--- modules/server/cozy.nix
+++ modules/server/cozy.nix
@@ -11,7 +11,7 @@
   environment = {
     DOMAIN = domain;
     COUCHDB_PROTOCOL = "http";
-    COUCHDB_HOST = "cozy-couchdb";
+    COUCHDB_HOST = "localhost";
     COUCHDB_PORT = toString config.services.couchdb.port;
     COUCHDB_USER = cfg.user;
     COZY_SUBDOMAIN_TYPE = cfg.subdomainType;
