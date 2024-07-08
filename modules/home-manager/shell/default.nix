c063e0a63251a37abde339717fff297e2210d310
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-07-08 14:58:14 +0200

    fix(shell): set shell to 256 colors

--- modules/home-manager/shell/default.nix
+++ modules/home-manager/shell/default.nix
@@ -76,6 +76,7 @@ in {
             # Vi mode
             export VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
             export VI_MODE_SET_CURSOR=true
+            export TERM=xterm-256color
           ''
 
           ''
