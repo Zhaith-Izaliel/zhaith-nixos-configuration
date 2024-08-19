abb02994bbba5f3b0845904607539f0d20de3056
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-08-19 20:04:40 +0200

    feat(notifications): update notifications options

--- modules/home-manager/desktop-environment/notifications.nix
+++ modules/home-manager/desktop-environment/notifications.nix
@@ -34,13 +34,27 @@ in {
     width = mkOption {
       type = types.ints.unsigned;
       default = 400;
-      description = "Set the width of the notifications client.";
+      description = "Defines the width of the notifications client.";
     };
 
-    height = mkOption {
-      type = types.ints.unsigned;
-      default = 400;
-      description = "Set the height of the notifications client.";
+    timeouts = {
+      normal = mkOption {
+        type = types.ints.unsigned;
+        default = 10;
+        description = "Defines the low priority notifications timeout.";
+      };
+
+      low = mkOption {
+        type = types.ints.unsigned;
+        default = 5;
+        description = "Defines the low priority notifications timeout.";
+      };
+
+      critical = mkOption {
+        type = types.ints.unsigned;
+        default = 0;
+        description = "Defines the low priority notifications timeout.";
+      };
     };
 
     position = mkOption {
@@ -57,6 +71,84 @@ in {
       ];
       default = "top-right";
       description = "Set the position of the notifications client.";
+    };
+
+    notifications-visibility = mkOption {
+      default = {};
+      description = ''
+        Set the visibility or override urgency of each incoming notification.
+
+        If the notification doesn't include one of the properties, that property will be ignored.
+        All properties (except for state) use regex.
+        If all properties match the given notification, the notification will be follow the provided state.
+        Only the first matching object will be used.
+      '';
+
+      example = ''
+        {
+          example-name = {
+            state = "The notification state";
+            app-name = Notification app-name Regex";
+            summary= "Notification summary Regex";
+            body= "Notification body Regex";
+            urgency= "Low or Normal or Critical";
+            category= "Notification category Regex";
+          };
+        }
+      '';
+    };
+
+    control-center = {
+      height = mkOption {
+        type = types.ints.unsigned;
+        default = 400;
+        description = ''
+          Defines the height of the notifications control center.
+
+          This setting is ignored when `fit-to-screen` is set to `true`.
+        '';
+      };
+
+      width = mkOption {
+        type = types.ints.unsigned;
+        default = 400;
+        description = "Defines the width of the notifications control client.";
+      };
+
+      fit-to-screen =
+        mkEnableOption null
+        // {
+          description = "Whether the control center should expand vertically to fill the screen.";
+        };
+
+      hide-on-clear =
+        mkEnableOption null
+        // {
+          description = "Hides the control center after pressing \"Clear All\"";
+        };
+
+      hide-on-action =
+        mkEnableOption null
+        // {
+          description = "Hides the control center when clicking on notification action.";
+          default = true;
+        };
+      keyboard-shortcuts =
+        mkEnableOption null
+        // {
+          description = "Whether the control center should use keyboard shortcuts.";
+          default = true;
+        };
+
+      relative-timestamps =
+        mkEnableOption null
+        // {
+          description = ''
+            Display notification timestamps relative to now e.g. "26 minutes ago".
+
+            If false, a local iso8601-formatted absolute timestamp is displayed.
+          '';
+        };
     };
   };
 
@@ -108,8 +200,19 @@ in {
       enable = true;
       # style = theme.swaync.style;
       settings = {
+        inherit (cfg.control-center) fit-to-screen relative-timestamps hide-on-clear hide-on-action keyboard-shortcuts;
+
         positionX = finalPositions.x;
         positionY = finalPositions.y;
+        layer = "overlay";
+        layer-shell = true;
+        cssPriority = "user";
+        notification-window-width = cfg.width;
+        timeout = cfg.timeouts.normal;
+        timeout-low = cfg.timeouts.low;
+        timeout-critical = cfg.timeouts.critical;
+        control-center-height = cfg.control-center.height;
+        control-center-width = cfg.control-center.width;
       };
     };
   };
