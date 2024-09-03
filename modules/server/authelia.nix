fa1ff051d48885700c224f611663da4ddeb1e881
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-09-03 10:43:14 +0200

    fix(authelia): fix nginx location for authelia

--- modules/server/authelia.nix
+++ modules/server/authelia.nix
@@ -143,7 +143,10 @@ in {
 
           endpoints = {
             enable_pprof = false;
+
             enable_expvars = false;
+
+            authz.auth-request.implementation = "AuthRequest";
           };
 
           disable_healthcheck = false;
@@ -312,33 +315,43 @@ in {
       locations = {
         "/" = {
           proxyPass = "http://localhost:${toString cfg.port}";
+          recommendedProxySettings = false;
 
           extraConfig = ''
+            ## Headers
+            proxy_set_header Host $host;
+            proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Host $http_host;
+            proxy_set_header X-Forwarded-URI $request_uri;
+            proxy_set_header X-Forwarded-Ssl on;
+            proxy_set_header X-Forwarded-For $remote_addr;
+            proxy_set_header X-Real-IP $remote_addr;
+
+            ## Basic Proxy Configuration
             client_body_buffer_size 128k;
+            proxy_next_upstream error timeout invalid_header http_500 http_502 http_503; ## Timeout if the real server is dead.
+            proxy_redirect  http://  $scheme://;
+            proxy_http_version 1.1;
+            proxy_cache_bypass $cookie_session;
+            proxy_no_cache $cookie_session;
+            proxy_buffers 64 256k;
 
-            #Timeout if the real server is dead
-            proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+            ## Trusted Proxies Configuration
+            ## Please read the following documentation before configuring this:
+            ##     https://www.authelia.com/integration/proxies/nginx/#trusted-proxies
+            # set_real_ip_from 10.0.0.0/8;
+            # set_real_ip_from 172.16.0.0/12;
+            # set_real_ip_from 192.168.0.0/16;
+            # set_real_ip_from fc00::/7;
+            real_ip_header X-Forwarded-For;
+            real_ip_recursive on;
 
-            # Advanced Proxy Config
+            ## Advanced Proxy Configuration
             send_timeout 5m;
             proxy_read_timeout 360;
             proxy_send_timeout 360;
             proxy_connect_timeout 360;
-
-            # Basic Proxy Config
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Uri $request_uri;
-            proxy_set_header X-Forwarded-Ssl on;
-            proxy_redirect  http://  $scheme://;
-            proxy_http_version 1.1;
-            proxy_set_header Connection "";
-            proxy_cache_bypass $cookie_session;
-            proxy_no_cache $cookie_session;
-            proxy_buffers 64 256k;
           '';
         };
 
