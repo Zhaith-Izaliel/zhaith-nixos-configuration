e645c01ede01c3172ae3fc6cd4f34f0d7298c426
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-09-13 10:10:37 +0200

    feat(Whispering-Willow): move to nvidia 560 and linux kernel zen

--- hosts/Whispering-Willow/default.nix
+++ hosts/Whispering-Willow/default.nix
@@ -26,7 +26,7 @@
   ];
 
   boot = {
-    # kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages_zen;
     initrd.kernelModules = ["i915"];
   };
 
@@ -99,25 +99,25 @@
       nvidia.nouveau.enable = false;
       nvidia.proprietary = {
         enable = true;
-        # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-        package = let
-          rcu_patch = pkgs.fetchpatch {
-            url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-            hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-          };
-        in
-          config.boot.kernelPackages.nvidiaPackages.mkDriver {
-            # Trying to fix the kernel panics related to NVidia,
-            # See: https://www.reddit.com/r/NixOS/comments/1dd7mj6/kernel_panic_with_nvidia_driver_550_on_laptop/
-            version = "535.154.05";
-            sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-            sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-            openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-            settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-            persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        # package = let
+        #   rcu_patch = pkgs.fetchpatch {
+        #     url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+        #     hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        #   };
+        # in
+        #   config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #     # Trying to fix the kernel panics related to NVidia,
+        #     # See: https://www.reddit.com/r/NixOS/comments/1dd7mj6/kernel_panic_with_nvidia_driver_550_on_laptop/
+        #     version = "535.154.05";
+        #     sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+        #     sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+        #     openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+        #     settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+        #     persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
 
-            patches = [rcu_patch];
-          };
+        #     patches = [rcu_patch];
+        #   };
         power-management.enable = true;
         modesetting.enable = true;
         deviceFilterName = "RTX 3060";
