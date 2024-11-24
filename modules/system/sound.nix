26099afd85e74680f27820c7835bcaeffa1a3d67
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-11-24 17:02:07 +0100

    feat(system/sound): add support for sound sharing over the network

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -1,14 +1,19 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
-  inherit (lib) mkIf mkEnableOption mkOption types;
+  inherit (lib) mkIf mkEnableOption mkOption types mkPackageOption optionalAttrs mkMerge optionals;
   cfg = config.hellebore.sound;
   toPeriod = quantum: "${toString quantum}/${toString cfg.lowLatency.rate}";
+  finalPackage = pkgs.pipewire.override (optionalAttrs cfg.soundSharing.enable {zeroconfSupport = true;});
+  isSoundSharingSender = builtins.any (item: item == cfg.soundSharing.mode) ["sender" "both"];
 in {
   options.hellebore.sound = {
     enable = mkEnableOption "Hellebore sound configuration";
+
+    package = mkPackageOption pkgs "pipewire" {};
 
     bluetoothEnhancements = mkEnableOption "the mSBC and SBC-XQ bluetooth codec in Wireplumber.";
 
@@ -42,34 +47,93 @@ in {
         PipeWire.";
       };
     };
-  };
 
-  config = mkIf cfg.enable {
-    security.rtkit.enable = true;
+    soundSharing = {
+      enable = mkEnableOption "sound sharing between computers over the network using pipewire-pulsaudio";
 
-    services.pipewire = {
-      enable = true;
-      wireplumber = {
-        enable = true;
+      mode = mkOption {
+        type = types.enum ["receiver" "sender" "both"];
+        default = "both";
+        description = "Defines if the current machine is a receiver, a sender, or both.";
       };
 
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
+      senderAddress = mkOption {
+        type = types.nonEmptyStr;
+        default = "";
+        description = "The local IP address of the sender. It is only used when your machine is sending sound to the network.";
+      };
+    };
+  };
 
-      wireplumber.extraConfig = {
-        bluetoothEnhancements = mkIf cfg.bluetoothEnhancements {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-          };
+  config = mkIf cfg.enable (mkMerge [
+    {
+      security.rtkit.enable = true;
+
+      services.pipewire = {
+        enable = true;
+        package = finalPackage;
+
+        wireplumber = {
+          enable = true;
+        };
+
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+
+      hardware = {
+        # IMPORTANT: disable pulseaudio when using pipewire
+        pulseaudio.enable = false;
+      };
+    }
+
+    (mkIf cfg.bluetoothEnhancements {
+      services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+        };
+      };
+    })
+
+    (mkIf cfg.soundSharing.enable {
+      services.avahi = {
+        enable = true;
+        openFirewall = true;
+
+        publish = mkIf isSoundSharingSender {
+          enable = true;
+          userServices = true;
         };
       };
 
-      extraConfig.pipewire."92-low-latency" = mkIf cfg.lowLatency.enable {
+      services.pipewire.extraConfig.pipewire-pulse."50-network-sharing" = {
+        pulse.cmd =
+          [
+            {
+              cmd = "load-module";
+              args = "module-zeroconf-discover";
+            }
+          ]
+          ++ optionals isSoundSharingSender [
+            {
+              cmd = "load-modules";
+              args = "module-native-protocol-tcp listen=${cfg.soundSharing.senderAddress}";
+            }
+            {
+              cmd = "load-modules";
+              args = "module-zeroconf-publish";
+            }
+          ];
+      };
+    })
+
+    (mkIf cfg.lowLatency.enable {
+      services.pipewire.extraConfig.pipewire."92-low-latency" = {
         context = {
           properties = {
             default.clock.rate = cfg.lowLatency.rate;
@@ -95,11 +159,6 @@ in {
           resample.quality = 1;
         };
       };
-    };
-
-    hardware = {
-      # IMPORTANT: disable pulseaudio when using pipewire
-      pulseaudio.enable = false;
-    };
-  };
+    })
+  ]);
 }
